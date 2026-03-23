2c501412d01e77001a85f134e9857b030f468f4c
Sergey Akishin <morales66@mail.ru>
2026-03-23 17:27:08 +0300

    test deploy

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export const Header = () => {
 	return (
 		<div className={styles.header}>
-			<h1>Чек-лист дел</h1>
+			<h1>Чек-лист дел 1</h1>
 			<p>Добавляйте, редактируйте, удаляйте и отмечайте выполненные дела</p>
 		</div>
 	)
